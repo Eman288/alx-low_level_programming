f80ab7869d748cc799bd41ffe02647578d8f0f81
Eman288 <emantamam107@gmail.com>
2023-02-16 14:26:37 -0800

    Task0-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main =  a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
