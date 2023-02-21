0de0994dfa5fbdef7298c1ab9c46c6456300f7de
Eman288 <emantamam107@gmail.com>
2023-02-21 12:57:39 -0800

    Task2-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
+#include <instd.h>
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+*main - a function that prints the alphabet,followed by a new line.
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+        char c = 'a';
+
+        while
+                (c <= 'z') {
+                        _putchar(c);
+                        c++;
+                }
+        _putchar('\n');
 }
+~          
