335259f019861fb140e6412d80f204349cc3950f
Eman288 <emantamam107@gmail.com>
2023-02-21 14:08:45 -0800

    Task2-3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - a function that checks for lowercase character.
+*Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+	if (n >= 65 && n <= 90)
+	{
+		return (0);
+	} else if (n >= 97 && n <= 122)
+	{
+		return (1);
+	}
+}
