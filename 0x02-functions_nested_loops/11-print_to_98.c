9f3c23c0bd72d41aa97dcd2c97d030d3c851273e
Eman288 <emantamam107@gmail.com>
2023-02-21 16:42:51 -0800

    Task2-11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_to_98 - prints all natural numbers
+*@n: a num
+*Return: Always 0 (Success)
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	} else
+	{
+		for (; n <= 98 ; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+}
