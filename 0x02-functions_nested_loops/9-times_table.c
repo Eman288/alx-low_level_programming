58c1f8da76f6972d631661d20cd688d16e05d7ac
Eman288 <emantamam107@gmail.com>
2023-02-21 16:25:48 -0800

    Task2-9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,14 +5,31 @@
 */
 void times_table(void)
 {
-	int n, m;
+	int n, m, u, d, z;
 
 	for (n = 0; n < 10; n++)
 	{
 		for (m = 0; m < 10; m++)
 		{
-			_putchar(n * m);
-			_putchar(' ');
+			z = n * m;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			} else
+			{
+				if (m != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
 		}
 		_putchar('\n');
 	}
