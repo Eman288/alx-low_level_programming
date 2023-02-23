a909c38ccaf2bf2bbf85d9be9b2e8247300dc8ff
Eman288 <emantamam107@gmail.com>
2023-02-23 13:54:56 -0800

    Task4-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - a function that prints the numbers
+*Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	char n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+}
