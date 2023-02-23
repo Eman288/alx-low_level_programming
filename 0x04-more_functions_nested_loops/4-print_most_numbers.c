11a409337b9d987d7456a2268ffd3e696f6ae0a3
Eman288 <emantamam107@gmail.com>
2023-02-23 14:14:19 -0800

    task4-4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,10 @@ void print_most_numbers(void)
 	n = '0';
 	while (n <= '9')
 	{
-		if (n == '2' || n == '4')
-			continue;
-		_putchar(n);
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
 		n++;
 	}
 	_putchar('\n');
