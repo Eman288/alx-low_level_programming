24f4fbf3310754695e0d51a44703293452a1f7b7
Eman288 <emantamam107@gmail.com>
2023-02-27 14:26:46 -0800

    Task5-3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_puts -  a function that prints a string
+*@str:a char
+*Return: Always 0 (Success)
+*/
+void _puts(char *str)
+{
+	while (*str != '\0)
+	{
+		_putchar(*str);
+		str++;
+	}
+}
