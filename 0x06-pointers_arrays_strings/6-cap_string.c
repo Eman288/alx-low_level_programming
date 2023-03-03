33fe7e623fc62eb0b00dafa22dbb7c28f9789a0e
Eman288 <emantamam107@gmail.com>
2023-03-02 17:29:06 -0800

    Task6-6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+*cap_string - a function that capitalizes all words of a string
+*@n: a char
+*Return: Always 0 (Success)
+*/
+char *cap_string(char *n)
+{
+	int m;
+
+	char *s;
+
+	s = n;
+	m = 0;
+	while (*n != '\0')
+	{
+		if (*n == '\n' || *n == '\t' || *n == ' ' || *n == ',' || *n == ';' ||
+				*n == '.' || *n == '?' || *n == '!' || *n == '"' ||
+				*n == '(' || *n == ')' || *n == '{' || *n == '}')
+		{
+			m = 0;
+		}
+		if (m == 0)
+		{
+			if (*n >= 97 && *n <= 122)
+			{
+				*n -= 32;
+			}
+			m++;
+		}
+		n++;
+	}
+	return (s);
+}
