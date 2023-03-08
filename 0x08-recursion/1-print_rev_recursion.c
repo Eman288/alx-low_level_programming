59141c8ec55b69ed3ea2d2d4a0ce05be23ac7520
Eman288 <emantamam107@gmail.com>
2023-03-08 12:14:00 -0800

    Task8-1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-	s++;
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
