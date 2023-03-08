fde2a57d80f8277b84d76f08d36923f0295b6974
Eman288 <emantamam107@gmail.com>
2023-03-08 11:30:36 -0800

    Task8-6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
 *is_prime_number - Write a function that returns
-*div - a fun that helps
+*divv - a fun that helps
 *@m: a num
 *@s: a num
 *@n: a num
 *Return: Always 0 (Success)
 */
-int div(int n, int m, int s)
+int divv(int n, int m, int s)
 {
 	if (m > n)
 	{
@@ -17,12 +17,11 @@ int div(int n, int m, int s)
 	{
 		s++;
 	}
-	div(n, m + 1, s);
-	return;
+	divv(n, m + 1, s);
 }
 int is_prime_number(int n)
 {
-	if (div(n, 1, 0) == 2)
+	if (divv(n, 1, 0) == 2)
 	{
 		return (1);
 	}
