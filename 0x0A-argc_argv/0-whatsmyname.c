a03384894e3fa6d5d8480ef25ba37ad4123381d8
Eman288 <emantamam107@gmail.com>
2023-03-10 10:03:08 -0800

    Task0A-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - Entry point
+*@argc: a num
+*@argv: a char
+*Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%c\n", argv[0]);
+	return (0);
+}
