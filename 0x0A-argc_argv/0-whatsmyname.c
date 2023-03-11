0525d25b7944fa153cc8448d6e78f1f072d1965f
Eman288 <emantamam107@gmail.com>
2023-03-11 07:01:08 -0800

    Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
 *@argv: a char
 *Return: Always 0 (Success)
 */
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
-	printf("%s\n", argv[argc]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
