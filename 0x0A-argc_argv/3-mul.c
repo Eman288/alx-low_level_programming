83a3119e000af5f99b472e133f610583c4debd9c
Eman288 <emantamam107@gmail.com>
2023-03-11 07:19:01 -0800

    Task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 int _atoi(char *s)
-{		
+{
 	int i, d, n, len, f, digit;
-	
-	i = 0;	
-	d = 0;	
+
+	i = 0;
+	d = 0;
 	n = 0;
-	len = 0;	
+	len = 0;
 	f = 0;
 	digit = 0;
 	while (s[len] != '\0')
 		len++;
-	while (i < len && f == 0)	
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
@@ -24,11 +24,11 @@ int _atoi(char *s)
 			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;	
+				break;
 			f = 0;
 		}
 
-		i++;	
+		i++;
 	}
 	if (f == 0)
 		return (0);
