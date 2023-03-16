e274079ff23178815dbcce49be4fcd8ed8790fdb
Eman288 <emantamam107@gmail.com>
2023-03-16 15:07:31 -0700

    Task0C-2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	n = malloc(nmemb * size);
 	if (n == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; i < size; i++)
 	{
 		*(n + i) = '0';
 	}
