60b2d0f5c46d0ccb3380b8dcafc056d73718bc3a
Eman288 <emantamam107@gmail.com>
2023-03-20 14:46:52 -0700

    Task0E-0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef dog_h
+#define dog_h
 /**
  * struct dog - Short description
  * @name: First member
@@ -12,3 +14,9 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
