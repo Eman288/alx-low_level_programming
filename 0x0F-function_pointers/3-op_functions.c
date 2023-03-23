5b405f3b5146d0b0f97ebc82785b5e41ffa4efdb
Eman288 <emantamam107@gmail.com>
2023-03-22 18:06:06 -0700

    Task0F-3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+*op_add - aadds two nums
+*@a: a num
+*@b: a num
+*Return: Always 0 (Success)
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+*op_sub - subs two nums
+*@a: anum
+*@b: a num
+*Return: Always 0 (Success)
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+*op_mul - muls two nums
+*@a: anum
+*@b: a num
+*Return: Always 0 (Success)
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+*op_div - divs two nums
+*@a: anum
+*@b: a num
+*Return: Always 0 (Success)
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+*op_mod - mods two nums
+*@a: anum
+*@b: a num
+*Return: Always 0 (Success)
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
