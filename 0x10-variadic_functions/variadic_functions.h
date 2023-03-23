b9050759bc892754c8b393ad7de44e8013465834
Eman288 <emantamam107@gmail.com>
2023-03-23 06:50:10 -0700

    Task10-0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
-#include <stdlib.h>
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
 #endif
