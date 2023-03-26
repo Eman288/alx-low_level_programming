cf39bffb1963edb011b21117823cb93461c5a6dc
Eman288 <emantamam107@gmail.com>
2023-03-26 14:39:30 -0700

    Task08-7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,21 @@
 #include <stdbool.h>
 #include "main.h"
 /**
+*count - to count letters
+*@i: a num
+*@y: a num
+*@c: a strin
+*Return: Always 0 (Success)
+*/
+int count(int i, char *c, int y)
+{
+	if (c[i] == '\0')
+		return (y);
+	y++;
+	i++;
+	return (count(i, c, y));
+}
+/**
 *check - Entry point
 *@flag: a flag
 *@c: the string
@@ -9,36 +24,32 @@
 *@i: a num
 *Return: Always 0 (Success)
 */
-int check(bool flag, char *c,int n, int i)
+int check(bool flag, char *c, int n, int i)
 {
-        if (i >= n / 2 || flag == false)
-                return (flag);
-        if (c[i] != c[n - 1 - i])
-        {
-                flag = false;
-                return (flag);
-        }else
-        {
-            i++;
-            check(flag,c,n,i);
-        }
-	        return(check(flag,c,n,i));
+	if (i >= n / 2 || flag == false)
+		return (flag);
+	if (c[i] != c[n - 1 - i])
+	{
+		flag = false;
+		return (flag);
+	}
+	else
+	{
+		i++;
+		check(flag, c, n, i);
+	}
+	return (check(flag, c, n, i));
 }
 /**
-*is_palindrome - a function that returns 1 if a string is a palindrome and 0 if not.
-*@S: the string
+*is_palindrome - a function that returns 1 if a string is a pal.
+*@s: the string
 *Return: Always 0 (Success)
 */
 int is_palindrome(char *s)
 {
-	int m, v;
+	int v;
 
-	m = 0;
-	while (s[m] != '\0')
-	{
-		m++;
-	}
-	v = check(true, s, m,0);
+	v = check(true, s, count(0, s, 0), 0);
 	if (v != 0)
 		return (1);
 	else
