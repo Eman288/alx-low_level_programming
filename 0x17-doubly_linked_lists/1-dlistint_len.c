ea720a58347d5922978d725986a46e23837063e3
Eman288 <emantamam107@gmail.com>
2023-08-18 18:05:41 -0700

    0x17-task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*print_dlistint - a function that prints all the elements of a dlistint_t list
+*@h: the head
+*Return: Always 0 (Success)
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int m;
+
+	m = 0;
+	while (h != NULL)
+	{
+		m++;
+		h = h->next;
+	}
+	return (m);
+}
