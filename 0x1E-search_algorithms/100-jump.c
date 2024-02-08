29c6494ab820b1f82d495eed429470b35e0c3f65
Eman288 <emantamam107@gmail.com>
2024-02-07 18:19:36 -0800

    betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	step = sqrt(size) ;
+	step = sqrt(size);
 	for (i = 0; (size_t) i < size; i += step)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
